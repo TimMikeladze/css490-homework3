93db01b76248022145389204daf431d3a0742363
Tim Mikeladze <tim.mikeladze@gmail.com>
2014-01-30 15:10:56 -0800

    finished layout

---
+++ src/com/css490/homework3/MainActivity.java
@@ -0,0 +1,46 @@
+
+package com.css490.homework3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void setSlow(View v) {
+
+    }
+
+    public void setMedium(View v) {
+
+    }
+
+    public void setFast(View v) {
+
+    }
+
+    public void setWalking(View v) {
+
+    }
+
+    public void setRunning(View v) {
+
+    }
+
+    public void setCycling(View v) {
+
+    }
+
+    public void calculate(View v) {
+
+    }
+
+}
